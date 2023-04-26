951c6b759c07b3db5bbbc2290196deafbd51c8a5
ali kamran <ali.kamran.98.98@gmail.com>
2023-04-26 19:27:47 +0330

    part1

--- app/src/main/java/com/example/databinding/MainActivity.kt
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding.submitButton.setOnClickListener {
+            displayGreeting()
+        }
+    }
+
+    private fun displayGreeting() {
+
+        binding.apply {
+            greetingTextView.text = "HELLO "+ editTextTextPersonName .text
+
+        }
+
     }
 }
